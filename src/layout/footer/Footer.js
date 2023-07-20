60384cc9e6a65b485d1a0ba429c7fd7e6ce44e24
umutsenturk34 <senturkumut3408@gmail.com>
2023-07-20 13:55:57 +0300

    our services interface has been made

--- src/layout/footer/Footer.js
+++ src/layout/footer/Footer.js
@@ -1,165 +1,180 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
-    return (
-        <>
-            <a href="#" className="to_top">
-                <i className="bi bi-chevron-up"></i>
-                <small>top</small>
-            </a>
-            <footer className="style-1">
-                <div className="container">
-                    <div className="content">
-                        <div className="row justify-content-center">
-                            <div className="col-lg-4">
-                                <div className="foot_info">
-                                    <a href="#" className="logo mb-3">
-                                        <img src="assets/img/logo_cl.png" alt="" />
-                                    </a>
-                                    <div className="text mb-4">
-                                        Best IT Solutions &amp; Technology WordPress <br /> Theme for Your Busines
-                                    </div>
-                                    <ul className="mb-4">
-                                        <li className="d-flex">
-                                            <i className="bi bi-house me-3" />
-                                            <a href="#">
-                                                <span>58 Howard St, San Francisco, CA 941</span>
-                                            </a>
-                                        </li>
-                                        <li className="d-flex">
-                                            <i className="bi bi-envelope me-3" />
-                                            <a href="#">
-                                                <span>contact@Iteck.co</span>
-                                            </a>
-                                        </li>
-                                        <li className="d-flex">
-                                            <i className="bi bi-phone me-3" />
-                                            <a href="#">
-                                                <span>(+23) 5535 68 68</span>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                    <div className="social_icons">
-                                        <a href="#">
-                                            <i className="fab fa-twitter" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fab fa-facebook-f" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fab fa-linkedin-in" />
-                                        </a>
-                                        <a href="#">
-                                            <i className="fab fa-github" />
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-2">
-                                <div className="links">
-                                    <div className="cont">
-                                        <h6 className="link_title">
-                                            Services
-                                        </h6>
-                                        <ul>
-                                            <li>
-                                                <a href="#">IT Consultations </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Data Security </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Website Development </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">UI/UX Design </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Cloud Services </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Game Development </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">CRM &amp; Software </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-2">
-                                <div className="links">
-                                    <div className="cont">
-                                        <h6 className="link_title">
-                                            Information
-                                        </h6>
-                                        <ul>
-                                            <li>
-                                                <a href="#">About Iteck </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Investors </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Blog </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Career </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Contact </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Affiliate Program </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">How It Works </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Pricing Plan </a>
-                                            </li>
-                                            <li>
-                                                <a href="#">Covid-19 Update </a>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-4">
-                                <div className="foot_subscribe">
-                                    <h6 className="link_title">
-                                        Newsletter
-                                    </h6>
-                                    <p>
-                                        Register now to get latest updates on  promotions &amp; coupons.
-                                    </p>
-                                    <div className="input-group my-4">
-                                        <input type="text" className="form-control" placeholder="Enter your email" aria-label="Enter your email" aria-describedby="button-addon2" />
-                                        <button className="btn butn-gard border-0 text-white px-3" type="button" id="button-addon2">
-                                            <span>Subscribe</span>
-                                        </button>
-                                    </div>
-                                    <p className="fst-italic">By subscribing, you accepted the our <a href="#" className="text-decoration-underline"> Policy</a></p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row justify-content-center">
-                        <div className="col-lg-12">
-                            <div className="foot">
-                                <p>
-                                    © 2022 Copyrights by <a href="#" className="text-white text-decoration-underline">Iteck Co.</a> All Rights Reserved by <a href="#" className="text-white text-decoration-underline"> ThemesCamp </a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+  return (
+    <>
+      <footer className="style-1">
+        <div className="container">
+          <div className="content">
+            <div className="row justify-content-center">
+              <div className="col-lg-4">
+                <div className="foot_info">
+                  <a href="#" className="logo mb-3">
+                    <img src="assets/img/logo_cl.png" alt="" />
+                  </a>
+                  <div className="text mb-4">
+                    Best IT Solutions &amp; Technology WordPress <br /> Theme
+                    for Your Busines
+                  </div>
+                  <ul className="mb-4">
+                    <li className="d-flex">
+                      <i className="bi bi-house me-3" />
+                      <a href="#">
+                        <span>58 Howard St, San Francisco, CA 941</span>
+                      </a>
+                    </li>
+                    <li className="d-flex">
+                      <i className="bi bi-envelope me-3" />
+                      <a href="#">
+                        <span>contact@Iteck.co</span>
+                      </a>
+                    </li>
+                    <li className="d-flex">
+                      <i className="bi bi-phone me-3" />
+                      <a href="#">
+                        <span>(+23) 5535 68 68</span>
+                      </a>
+                    </li>
+                  </ul>
+                  <div className="social_icons">
+                    <a href="#">
+                      <i className="fab fa-twitter" />
+                    </a>
+                    <a href="#">
+                      <i className="fab fa-facebook-f" />
+                    </a>
+                    <a href="#">
+                      <i className="fab fa-linkedin-in" />
+                    </a>
+                    <a href="#">
+                      <i className="fab fa-github" />
+                    </a>
+                  </div>
                 </div>
-                <img src="assets/img/footer/foot_l.png" alt="" className="foot_l" />
-                <img src="assets/img/footer/foot_r.png" alt="" className="foot_r" />
-            </footer>
-        </>
-
-    )
-}
+              </div>
+              <div className="col-lg-2">
+                <div className="links">
+                  <div className="cont">
+                    <h6 className="link_title">Services</h6>
+                    <ul>
+                      <li>
+                        <a href="#">IT Consultations </a>
+                      </li>
+                      <li>
+                        <a href="#">Data Security </a>
+                      </li>
+                      <li>
+                        <a href="#">Website Development </a>
+                      </li>
+                      <li>
+                        <a href="#">UI/UX Design </a>
+                      </li>
+                      <li>
+                        <a href="#">Cloud Services </a>
+                      </li>
+                      <li>
+                        <a href="#">Game Development </a>
+                      </li>
+                      <li>
+                        <a href="#">CRM &amp; Software </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-2">
+                <div className="links">
+                  <div className="cont">
+                    <h6 className="link_title">Information</h6>
+                    <ul>
+                      <li>
+                        <a href="#">About Iteck </a>
+                      </li>
+                      <li>
+                        <a href="#">Investors </a>
+                      </li>
+                      <li>
+                        <a href="#">Blog </a>
+                      </li>
+                      <li>
+                        <a href="#">Career </a>
+                      </li>
+                      <li>
+                        <a href="#">Contact </a>
+                      </li>
+                      <li>
+                        <a href="#">Affiliate Program </a>
+                      </li>
+                      <li>
+                        <a href="#">How It Works </a>
+                      </li>
+                      <li>
+                        <a href="#">Pricing Plan </a>
+                      </li>
+                      <li>
+                        <a href="#">Covid-19 Update </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col-lg-4">
+                <div className="foot_subscribe">
+                  <h6 className="link_title">Newsletter</h6>
+                  <p>
+                    Register now to get latest updates on promotions &amp;
+                    coupons.
+                  </p>
+                  <div className="input-group my-4">
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Enter your email"
+                      aria-label="Enter your email"
+                      aria-describedby="button-addon2"
+                    />
+                    <button
+                      className="btn butn-gard border-0 text-white px-3"
+                      type="button"
+                      id="button-addon2"
+                    >
+                      <span>Subscribe</span>
+                    </button>
+                  </div>
+                  <p className="fst-italic">
+                    By subscribing, you accepted the our{" "}
+                    <a href="#" className="text-decoration-underline">
+                      {" "}
+                      Policy
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            <div className="col-lg-12">
+              <div className="foot">
+                <p>
+                  © 2022 Copyrights by{" "}
+                  <a href="#" className="text-white text-decoration-underline">
+                    Iteck Co.
+                  </a>{" "}
+                  All Rights Reserved by{" "}
+                  <a href="#" className="text-white text-decoration-underline">
+                    {" "}
+                    ThemesCamp{" "}
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <img src="assets/img/footer/foot_l.png" alt="" className="foot_l" />
+        <img src="assets/img/footer/foot_r.png" alt="" className="foot_r" />
+      </footer>
+    </>
+  );
+};
 
 export default Footer;
---
+++ src/pages/hizmetlerimiz.js
@@ -0,0 +1,44 @@
+import { Content } from "@/components/Content";
+import Services from "@/components/home/Services";
+
+import React from "react";
+
+function hizmetlerimiz() {
+  return (
+    <>
+      <Content
+        title={"Hizmetlerimiz"}
+        desc={"İşinizi Kolaylaştıran, İnovatif Çözümler…"}
+      >
+        <div className="container mt-6">
+          <div className="row">
+            <div class="col-lg-6">
+              <div class="img mb-4 mb-lg-0 wow fadeIn">
+                <img src="assets/img/about/3d_vector2.svg" alt=""></img>
+              </div>
+            </div>
+            <div className="col-lg-6">
+              <div>
+                <h2 className="">
+                  Geniş Hizmet Yelpazesi Sunuyor ve Tam Müşteri Memnuniyeti
+                  Sağlıyoruz
+                </h2>
+                <p className="mt-3 fs-5">
+                  MegaCode Yazılım ile teknolojinin hızlı gelişiminin takibine
+                  ve kalitenin önceliğine inanan Türkiye’nin ilk ve tek sigorta
+                  temalı dijital ajansı olarak hizmet veriyoruz ve dijital
+                  dünyanın tüm yenilikçi çözümlerini işinizi büyütmek için
+                  kullanıyoruz.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <Services></Services>
+        
+      </Content>
+    </>
+  );
+}
+
+export default hizmetlerimiz;
