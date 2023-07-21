fef3205434294c0abf9423e51cc21b264082e94a
sfurkanpehlivan <131876054+sfurkanpehlivan@users.noreply.github.com>
2023-07-21 14:23:36 +0300

    career pages created

--- src/pages/career/index.js
+++ src/pages/career/index.js
@@ -1,10 +1,19 @@
 import Banner from '@/components/career/Banner'
+import CareerForm from '@/components/career/CareerForm'
+import CareerPositions from '@/components/career/CareerPositions'
+import { Content } from '@/components/Content'
 import React from 'react'
 
 const Career = () => {
     return (
         <>
-            <Banner />
+            <main className='careers-page style-5'>
+                <Content hideTitle title="Career">
+                    <Banner />
+                    <CareerPositions />
+                </Content>
+                <CareerForm />
+            </main>
         </>
     )
 }
