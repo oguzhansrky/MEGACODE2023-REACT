918fc4a945fc658b348c2c5a2032a106162cdb29
sfurkanpehlivan <131876054+sfurkanpehlivan@users.noreply.github.com>
2023-07-27 11:03:31 +0300

    +++

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,15 +1,8 @@
 import LoadingUI from "@/components/LoadingUI";
-
 import "@/styles/globals.css";
-import { useEffect, useState } from "react";
 import LayoutProvider from "@/layout/LayoutProvider";
 import { useRouter } from "next/router";
-
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import LayoutProvider from "@/layout/LayoutProvider";
-import "@/styles/globals.css";
-
 import { Provider } from "react-redux";
 import { store, persistor } from "../store/store";
 import { PersistGate } from "redux-persist/integration/react";
