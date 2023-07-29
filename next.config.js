8ea3d9130ec68f1def1f174fe9a2416457a3a47e
umutsenturk34 <senturkumut3408@gmail.com>
2023-07-29 10:16:54 +0300

    digitalsigna responsive

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 const nextTranslate = require("next-translate-plugin");
-const sitemap = require("next-sitemap");
-
-module.exports = {
-  siteUrl: "megacode.com.tr", // Buraya sitenizin adresini girin
-  generateRobotsTxt: true,
-  // Diğer yapılandırma seçeneklerini eklemek için burayı kullanabilirsiniz.
-};
 
 module.exports = nextTranslate({
   reactStrictMode: true,
